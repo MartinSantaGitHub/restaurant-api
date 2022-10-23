d4092547e47f334df8f641da263d3d6364f53ea4
martins <santasan27@gmail.com>
2022-10-23 11:23:07 -0300

    Change mocking of env variables

--- app/database/config.test.js
+++ app/database/config.test.js
@@ -1,7 +1,19 @@
-import { expect } from "vitest";
+import { afterEach, beforeEach, expect, vi } from "vitest";
 import { dbConnection } from "./config.js";
 
 describe("config", () => {
+    const env = process.env;
+
+    beforeEach(() => {
+        vi.resetModules();
+
+        process.env = { ...env };
+    });
+
+    afterEach(() => {
+        process.env = env;
+    });
+
     it("Should not throw error -> dbConnection", async () => {
         process.env.MONGODB_CONNSTRING = "mongodb://db";
 
